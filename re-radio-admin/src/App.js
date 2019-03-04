7e7d712f4a458aa89785b89dbc85422099b93a08
vtlu <vinh.thanh.lu@mgm-tp.com>
2019-03-04 19:28:57 +0700

    Initialize admin

---
+++ re-radio-admin/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+    </Admin>
+);
+
+export default App;
