6e6830395c42066a82c80a846d73ba9bf9261dd9
Tri Pham <pvtri96@gmail.com>
2019-03-05 18:30:09 +0700

    Add README and add dev.sh script to start the whole application

---
+++ dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Merge other mono repository dotenv files into 1 global file
+cat server/.env client/.env > .env
+
+base="docker-compose \
+  -f docker-compose.yml \
+  -f ./server/docker-compose.yml \
+  -f ./server/docker-compose.dev.yml \
+  -f ./server/database/docker-compose.yml \
+  -f ./server/database/docker-compose.mongo.yml \
+  -f ./client/docker-compose.dev.yml \
+  -f docker-compose.dev.yml \
+"
+case "$1" in
+'up' | 'down' | 'start' | 'stop' | 'config')
+  eval "$base $1"
+;;
+*)
+  echo $"Incorrect usage: It should be one of start, stop, up, down, config. For example:"
+  echo $"$0 start"
+  exit 1
+esac
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd server 
+npm install
+
+cd ..
+cd client 
+npm install
