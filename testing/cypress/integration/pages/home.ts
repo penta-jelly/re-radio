783356ef19984c66ed0b5031d09941c2cebc7157
dungle1811 <dungle1811@gmail.com>
2019-03-07 14:42:02 +0700

    Config end to end testing using Cypress

---
+++ testing/cypress/integration/pages/home.ts
@@ -0,0 +1,12 @@
+import { Search } from './search';
+
+export class Home {
+	navigate() {
+		cy.visit('https://css-tricks.com/');
+		return this;
+	}
+
+	openSearch() {
+		return new Search();
+	}
+}
---
+++ testing/cypress/integration/pages/result.ts
@@ -0,0 +1,12 @@
+export class Result {
+	assertUrl(term: string) {
+		const query = `?s=${term}`;
+		cy.url().should('include', query);
+
+		cy.get('nav.breadcrumbs')
+			.find('ul').children()
+			.should('have.length', 7);
+
+		cy.get('span.number-of-articles').contains('180 Articles').should('exist');
+	}
+}
