3bdf9d5c591dc9b4f8f2cdc3c0ef4a74add04cca
Nhat Khanh <lednhatkhanh@users.noreply.github.com>
2019-03-17 23:06:40 +0700

    feat: Migrate to react-router-dom and update all deps (#24)

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { mount, route } from 'navi';
 
 const About = () => <div>Hello from about page</div>;
 
-export default mount({
-  '/': route({
-    async getView(_request) {
-      return <About />;
-    },
-  }),
-});
+export default About;
