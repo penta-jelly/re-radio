c2e9e77fb5c861be45a153a1b540ed952768ec37
pvtri96 <pvtri96@gmail.com>
2019-04-13 11:06:31 +0700

    feat: stations navigation

--- client/src/modules/station/header/index.tsx
+++ client/src/modules/station/header/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Typography, Card } from '@material-ui/core';
+import { Typography, Card, CircularProgress } from '@material-ui/core';
 import { useStyles } from './styles';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { useStationQuery } from '../../../graphql';
 
-export const Header: React.FC<{}> = () => {
+interface RouteParams {
+  slug: string;
+}
+
+const BaseHeader: React.FC<RouteComponentProps<RouteParams>> = props => {
   const classes = useStyles();
+  const { data, loading, error } = useStationQuery({ variables: { slug: props.match.params.slug } });
+
+  const content = React.useMemo<React.ReactNode>(() => {
+    if (error) {
+      return error.message;
+    }
+    if (loading) {
+      return <CircularProgress />;
+    }
+    return data && data.station && data.station.name;
+  }, [data, loading, error]);
 
   return (
     <Card className={classes.container}>
-      <Typography variant="subtitle1">Header</Typography>
+      <Typography variant="subtitle1">{content}</Typography>
     </Card>
   );
 };
+
+export const Header = withRouter(BaseHeader);
--- client/src/pages/station.tsx
+++ client/src/pages/station.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { StationLayout } from '../modules';
 import { makeStyles, Theme } from '@material-ui/core';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { Layout } from '../containers/layout';
+import { StationLayout } from '../modules';
 
 const useStyles = makeStyles(({ spacing }: Theme) => ({
   container: {
@@ -10,13 +12,19 @@ const useStyles = makeStyles(({ spacing }: Theme) => ({
   },
 }));
 
-const Station: React.FC = () => {
+interface RouteParams {
+  slug: string;
+}
+
+const Station: React.FC<RouteComponentProps<RouteParams>> = props => {
   const classes = useStyles();
   return (
-    <div className={classes.container}>
-      <StationLayout />
-    </div>
+    <Layout drawer={{ open: true, collapsed: true }}>
+      <div className={classes.container}>
+        <StationLayout />
+      </div>
+    </Layout>
   );
 };
 
-export default Station;
+export default withRouter(Station);
