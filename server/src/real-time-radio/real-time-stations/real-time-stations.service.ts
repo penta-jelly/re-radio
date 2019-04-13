7fe54c5fba5c9880f27087fb1f540d85735eacb8
Tri Pham <pvtri96@gmail.com>
2019-04-13 22:11:32 +0700

    feat: enhance ability to recover from crashes (#88)
    - Real time service won't be affected by restarting of other services
    - Pending song will be moved to playing if there is no playing song

--- server/src/real-time-radio/real-time-stations/real-time-stations.service.ts
+++ server/src/real-time-radio/real-time-stations/real-time-stations.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { RealTimeSongService } from '../real-time-songs/real-time-songs.service';
 
 @Injectable()
-export class RealTimeStationsService {}
+export class RealTimeStationService {
+  private readonly logger = new Logger(RealTimeStationService.name);
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject(forwardRef(() => RealTimeSongService))
+    private readonly realTimeSongService: RealTimeSongService,
+  ) {}
+
+  async findAllAvailableStations(query?: string) {
+    return this.prisma.query.stations({}, query || `{ id name slug description createdAt updatedAt }`);
+  }
+
+  async isStationReadyToPlayNextSong(stationId: string) {
+    const playingSong = await this.realTimeSongService.findPlayingSongInStation(stationId);
+    if (playingSong) {
+      return false;
+    }
+
+    const pendingSongs = await this.prisma.query.songs({ where: { station: { id: stationId }, status: 'PENDING' } });
+    if (pendingSongs.length === 0) {
+      return false;
+    }
+    return true;
+  }
+}
