d29b58e7984bac6928926337ba59becdf397cd8d
Khanh Le <lednhatkhanh@gmail.com>
2019-04-14 17:33:28 +0700

    chore: Remove storybook

--- client/src/components/button/button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import { PrimaryButton } from './primary-button';
-
-storiesOf('Button', module).add('Primary', () => (
-  <PrimaryButton onClick={action('button clicked')}>Hello World</PrimaryButton>
-));
