42b74553bd336c34964cfabca32eba1203c76fdc
Tri Pham <pvtri96@gmail.com>
2019-08-22 14:18:00 +0700

    Fix typing error in Fs.ReadStream

--- server/src/core/files/files.service.ts
+++ server/src/core/files/files.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as Fs from 'fs';
-import { ExtendedReadStream } from 'graphql-upload';
 import * as Path from 'path';
 
 @Injectable()
 export class FilesService {
-  async storeFile(stream: ExtendedReadStream, filename: string): Promise<ReturnType> {
+  async storeFile(stream: Fs.ReadStream, filename: string): Promise<ReturnType> {
     // TODO: Store file on some cloud storage instead of the local disk
     return this.storeFileToLocalDisk(stream, filename);
   }
 
-  async storeFileToLocalDisk(stream: ExtendedReadStream, filename: string): Promise<ReturnType> {
+  async storeFileToLocalDisk(stream: Fs.ReadStream, filename: string): Promise<ReturnType> {
     const id = `${Date.now()}`;
     const hashedFilename = `${id}-${filename}`;
     const storagePath = this.getImagesLocalStoragePath();
@@ -18,7 +17,7 @@ export class FilesService {
     return new Promise((resolve, reject) =>
       stream
         .on('error', error => {
-          if (stream.truncated) {
+          if ((stream as { truncated?: boolean }).truncated) {
             // Delete the truncated file.
             Fs.unlinkSync(path);
           }
