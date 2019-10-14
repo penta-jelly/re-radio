d1ea4cf6428806dc6f8f827104a6c4049457ebc1
Tri Pham <pvtri96@gmail.com>
2019-10-14 11:51:44 +0700

    feat: add stress test seeder service (#289)

--- server/src/workers/seeder/seeder.module.ts
+++ server/src/workers/seeder/seeder.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { SeederService } from './seeder.service';
+import { StressTestSeederService } from './seeder.stress-test.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [SeederService],
-  exports: [SeederService],
+  providers: [SeederService, StressTestSeederService],
+  exports: [SeederService, StressTestSeederService],
 })
 export class SeederModule {}
