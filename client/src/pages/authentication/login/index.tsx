d51eed1f1a5d9666be218fed6569bdd957a3db19
Tri Van Pham <pvtri96@gmail.com>
2022-01-08 21:49:32 +0700

    fix: change login required notification position

--- client/src/pages/authentication/login/index.tsx
+++ client/src/pages/authentication/login/index.tsx
@@ -14,7 +14,12 @@ const Login: React.FC<RouteComponentProps> = ({ history }) => {
   React.useEffect(() => {
     if (currentUserQuery.data) {
       history.push('/');
-      enqueueSnackbar('You have to logout first to access this page.', { preventDuplicate: true, variant: 'warning' });
+      const options = { 
+        anchorOrigin: { vertical: 'top', horizontal: 'left' },
+        preventDuplicate: true,
+        variant: 'warning'
+      }
+      enqueueSnackbar('You have to logout first to access this page.', options);
     }
   }, [enqueueSnackbar, history, currentUserQuery]);
 
